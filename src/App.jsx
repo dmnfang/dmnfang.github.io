5611f7f966c87655e0a7957741c9e5c851f285b5
dmnfang <chris.rosier@gmail.com>
2026-06-02 07:01:28 +0900

    update hrefs to hiroba.ca

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import './App.css'
 
 const FRONTEND_PRODUCTS = [
-  { name: 'Cardbox',        desc: 'A collection of flashcard games for the classroom',          icon: 'ti-cards',        iconClass: 'icon-cardbox',      href: '#' },
-  { name: 'Momotaro',       desc: 'A unit review game',                                         icon: 'ti-sword',        iconClass: 'icon-momotaro',     href: 'https://dmnfang.github.io/momotaro-v2/' },
-  { name: 'Lucky Card Game',desc: 'A companion app to the Lucky Card physical card game',        icon: 'ti-star',         iconClass: 'icon-luckycard',    href: 'https://dmnfang.github.io/luckycard/' },
-  { name: 'Flashcount',     desc: 'An app for practicing counting',                              icon: 'ti-123',          iconClass: 'icon-flashcount',   href: '#' },
-  { name: 'Sky Clock',   desc: 'A sky-based clock for teaching the time',        icon: 'ti-clock',       iconClass: 'icon-skyclock',   href: 'https://hiroba.ca/skyclock/' },
-  { name: 'Date Flash',  desc: 'A flashcard app for months, dates and holidays',  icon: 'ti-calendar',    iconClass: 'icon-dateflash',  href: 'https://hiroba.ca/dateflash/' },
-  { name: 'Quiz Hub',       desc: 'A collection of classroom quiz games',                        icon: 'ti-layout-grid',  iconClass: 'icon-schedulequiz', href: 'https://dmnfang.github.io/quiz-hub/' },
-  { name: 'Sign Hunt',      desc: 'A signature collecting challenge app',                        icon: 'ti-user-search',  iconClass: 'icon-teacherhunt',  href: '#' },
+  { name: 'Cardbox',        desc: 'A collection of flashcard games for the classroom',         icon: 'ti-cards',        iconClass: 'icon-cardbox',      href: 'hiroba.ca/cardbox/' },
+  { name: 'Momotaro',       desc: 'A unit review game',                                        icon: 'ti-sword',        iconClass: 'icon-momotaro',     href: 'https://hiroba.ca/momotaro-v2/' },
+  { name: 'Lucky Card Game',desc: 'A companion app to the Lucky Card physical card game',       icon: 'ti-star',         iconClass: 'icon-luckycard',    href: 'https://hiroba.ca/luckycard/' },
+  { name: 'Flashcount',     desc: 'An app for practicing counting',                             icon: 'ti-123',          iconClass: 'icon-flashcount',   href: 'hiroba.ca/flashcount/' },
+  { name: 'Sky Clock',      desc: 'A sky-based clock for teaching the time',                   icon: 'ti-clock',        iconClass: 'icon-skyclock',     href: 'https://hiroba.ca/skyclock/' },
+  { name: 'Date Flash',     desc: 'A flashcard app for months, dates and holidays',             icon: 'ti-calendar',     iconClass: 'icon-dateflash',    href: 'https://hiroba.ca/dateflash/' },
+  { name: 'Quiz Hub',       desc: 'A collection of classroom quiz games',                       icon: 'ti-layout-grid',  iconClass: 'icon-schedulequiz', href: 'https://hiroba.ca/quiz-hub/' },
+  { name: 'Sign Hunt',      desc: 'A signature collecting challenge app',                       icon: 'ti-user-search',  iconClass: 'icon-teacherhunt',  href: '#' },
 ]
 
 const BACKEND_PRODUCTS = [
